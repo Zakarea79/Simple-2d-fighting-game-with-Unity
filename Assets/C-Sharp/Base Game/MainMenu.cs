cce0e80cb75356b5708f4687f0b00475976daf64
Mohammadhosseinjadidi <98258065+Mohammadhosseinjadidi@users.noreply.github.com>
2022-01-29 11:48:27 +0330

    Add main menu

---
+++ Assets/C-Sharp/Base Game/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    private GameObject topcanvas;
+    public void Click(GameObject canvas)
+    {
+        //فعال کردن بخشی که کلیک شده
+        canvas.SetActive(true);
+        //canvas تنظیم بالا ترین
+        topcanvas = canvas;
+    }
+    void Update()
+    {
+        if (topcanvas != null)
+        {
+            if(Input.GetKeyDown(KeyCode.Escape))
+            {
+                topcanvas.SetActive(false);
+            }
+        }
+    }
+}

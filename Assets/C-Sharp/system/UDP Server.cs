8c3e7d13f31a3416a822359d1fcf375684ad6c71
Zakarea <98216446+Zakarea79@users.noreply.github.com>
2022-01-29 14:35:31 +0330

    Update UDP Server.cs

--- Assets/C-Sharp/system/UDP Server.cs
+++ Assets/C-Sharp/system/UDP Server.cs
@@ -1,14 +1,9 @@
 ﻿using System;
 using System.Net;
 using System.Text;
-using System.Linq;
 using System.Net.Sockets;
-using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-using System.Threading;
-using System.Timers;
-using UnityEngine.UI;
+
 
 namespace Supernova_Server
 {
@@ -27,74 +22,6 @@ namespace Supernova_Server
 			}
 			return ipAdress;
 		}
-		//-------------------------------------------------------------------
-		//-------------------------------------------------------------------
-		//-------------------------------------------------------------------
-		//-------------------------------------------------------------------
-		//-------------------------------------------------------------------
-		//-------------------------------------------------------------------
-		//Socket so1;
-		//public void INITSERVER(string IPAderssV , int port)
-		//{
-		//	so1 = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-		//	so1.Bind(new IPEndPoint(IPAddress.Parse(IPAderssV), port));
-		//}
-		//public string GetDataServerUDP()
-		//{
-		//	byte[] buffer = new byte[2048];
-		//	int r = so1.Receive(buffer);
-		//	if (r > 0)
-		//	{
-		//		return (Encoding.Unicode.GetString(buffer, 0, r));
-		//	}
-		//	else
-		//	{
-		//		return "";
-		//	}
-		//}
-		
-		//public void ServerSendUDP(String SendData ,string IPAddressV , int port)
-		//{
-		//	byte[] buffer = new byte[2048];
-		//	buffer = Encoding.Unicode.GetBytes(SendData);
-		//	so1.SendTo(buffer, new IPEndPoint(IPAddress.Parse(IPAddressV), port));
-		//}
-		////-------------------------------------------------------------------
-		////-------------------------------------------------------------------
-		////-------------------------------------------------------------------
-		//Socket so2;
-		//public void INITCLIENT(string IPAderssV , int port)
-		//{
-		//	so2 = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-		//	so2.Bind(new IPEndPoint(IPAddress.Parse(IPAderssV), port));
-		//}
-		
-		//public string CientRseveUDP()
-		//{
-		//	byte[] buffer = new byte[2048];
-		//	int r = so2.Receive(buffer);
-		//	if (r > 0)
-		//	{
-		//		return Encoding.Unicode.GetString(buffer, 0, r);
-		//	}
-		//	else
-		//	{
-		//		return "";
-		//	}
-		//}
-		//public void ClientSendUDP(String SendData ,string IPAddressV , int port)
-		//{
-		//	byte[] buffer = new byte[2048];
-		//	buffer = Encoding.Unicode.GetBytes(SendData);
-		//	so2.SendTo(buffer, new IPEndPoint(IPAddress.Parse(IPAddressV), port));
-		//}
-		//-------------------------------------------------------------------
-		//-------------------------------------------------------------------
-		//-------------------------------------------------------------------
-		//-------------------------------------------------------------------
-		//-------------------------------------------------------------------
-		//-------------------------------------------------------------------
-		//-------------------------------------------------------------------
 		private IPEndPoint ipeServer;
 		public UdpClient UDPServer;
 		
@@ -126,8 +53,6 @@ namespace Supernova_Server
 			byte[] b2 = new byte[byteLength];
 			b2 = ASCIIEncoding.UTF8.GetBytes(Send);
 			UDPServer.Send(b2 , b2.Length , ipeServer);
-			
-			UDPServer.Close();
 		}
 		//-------------------------------------------------------------------
 		public void ServerSendData(string Send , int byteLength = 255)

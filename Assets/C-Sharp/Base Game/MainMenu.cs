c87b99bd185a36bf7bd6e3d90d1db7aa8684b3ff
Mohammadhosseinjadidi <98258065+Mohammadhosseinjadidi@users.noreply.github.com>
2022-01-30 09:42:51 +0330

    Update MainMenu.cs

--- Assets/C-Sharp/Base Game/MainMenu.cs
+++ Assets/C-Sharp/Base Game/MainMenu.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
     private GameObject topcanvas;
+    public Button firstbtn;
     public void Click(GameObject canvas)
     {
         //فعال کردن بخشی که کلیک شده
         canvas.SetActive(true);
         //canvas تنظیم بالا ترین
         topcanvas = canvas;
+    }
+    private void Start()
+    {
+        //انتخاب اولین دکمه تا بتوان با دکمه کیبرد انتخاب رو عوض کرد
+        firstbtn.Select();
     }
     void Update()
     {

fb45f35f1a417dac307992e978f46dd1fd4e6e0c
unknown <zakarea6688@gmail.com>
2022-02-03 08:22:50 +0330

    Carate Touch Control Script

---
+++ Assets/C-Sharp/Toch/Touch Cntrol.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ZInput
+{
+    public static bool GetKeyDown(KeyCode key)
+    {
+        return false;
+    }
+
+    public static bool GetKeyUp(KeyCode key)
+    {
+        return false;
+    }
+
+    public static bool GetKeyPress(KeyCode key)
+    {
+        return false;
+    }
+
+    public static bool GetKeyDown(string key)
+    {
+        return false;
+    }
+
+    public static bool GetKeyUp(string key)
+    {
+        return false;
+    }
+
+    public static bool GetKeyPress(string key)
+    {
+        return false;
+    }
+
+}

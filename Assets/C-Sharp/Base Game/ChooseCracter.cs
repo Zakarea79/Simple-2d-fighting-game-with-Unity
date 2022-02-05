9236dac546126a252499d060faa7f9da75ed37f9
Mohammadhosseinjadidi <98258065+Mohammadhosseinjadidi@users.noreply.github.com>
2022-02-05 11:46:50 +0330

    Update ChooseCracter.cs

--- Assets/C-Sharp/Base Game/ChooseCracter.cs
+++ Assets/C-Sharp/Base Game/ChooseCracter.cs
@@ -1,10 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class ChooseCracter : MonoBehaviour
 {
     private GameObject sprite;
+    private void Start()
+    {
+        PlayerPrefs.SetString("EnemyCracter", "Dadghah"); 
+
+        Debug.Log(PlayerPrefs.GetString("EnemyCracter")); 
+        Enemy(PlayerPrefs.GetString("EnemyCracter"));
+    }
+    void Enemy(string name)
+    {
+        GameObject.Find(name).gameObject.GetComponent<Image>().enabled = true;
+    }
     public void Choose(GameObject cracter)
     {
         if (sprite != null)
@@ -14,8 +26,8 @@ public class ChooseCracter : MonoBehaviour
         cracter.SetActive(true);
         sprite = cracter;
     }
-    public void Ready()
+    public void Ready(string scenename)
     {
-
+        PlayerPrefs.SetString("PlayerCracter",sprite.name);
     }
 }
